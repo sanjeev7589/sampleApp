3d8cd080d41b5314c44d673382aeb2766857a2ef
sanjeev7589 <sanjeev.ngm7589@gmail.com>
2023-11-20 10:19:15 +0530

    Added Login screen

--- sampleapp/webapp/controller/Login.controller.js
+++ sampleapp/webapp/controller/Login.controller.js
@@ -6,9 +6,10 @@ sap.ui.define([
     'sap/ui/core/message/Message',
     'sap/ui/core/library',
     'sap/ui/core/Core',
-    'sap/ui/core/Element'
+    'sap/ui/core/Element',
+    'sap/m/MessageToast'
     // 'com/app/customerportal/controller/Constant',
-], function (BaseController, JSONModel, MessagePopover, MessageItem, Message, coreLibrary, Core, Element, ErrorMessage) {
+], function (BaseController, JSONModel, MessagePopover, MessageItem, Message, coreLibrary, Core, Element, MessageToast, ErrorMessage) {
     "use strict";
     // var timerId, that;
     // // shortcut for sap.ui.core.MessageType
@@ -16,19 +17,19 @@ sap.ui.define([
 
     return BaseController.extend("com.sampleapp.controller.Login", {
         onInit: function () {
-            that = this;
-            that.getView().addStyleClass(that.getOwnerComponent().getContentDensityClass());
+            var that = this;
+            // that.getView().addStyleClass(that.getOwnerComponent().getContentDensityClass());
             //that.getView().setModel(new JSONModel(), "loginMdl");
             //that.showChatBot(false);
             let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.getRoute("login").attachMatched(this._onObjectMatched, this);
 
             //validation related id parameters
-            this.formId = this.getView().byId('loginForm');
-            this.pageId = this.getView().byId('loginPage');
-            this.popoverBtn = this.getView().byId('messagePopoverBtnLogin');
-            //
-            this.loginBtn = this.getView().byId("loginBtn");
+            // this.formId = this.getView().byId('loginForm');
+            // this.pageId = this.getView().byId('loginPage');
+            // this.popoverBtn = this.getView().byId('messagePopoverBtnLogin');
+            // //
+            // this.loginBtn = this.getView().byId("loginBtn");
             var oModel = new JSONModel({ email: "", password: "", enable: true })
             this.oView = this.getView();
             this.oView.setModel(oModel, "loginModel");
@@ -36,10 +37,10 @@ sap.ui.define([
         _onObjectMatched: function () {
             // this.onPressChangePassword();
             //Default theme
-            sap.ui.getCore().applyTheme("sap_horizon");
-            //value state removing if existing state is thare means
-            this.eMdl = this.getOwnerComponent().getModel('errors');
-            ErrorMessage.removeValueState(this.formId, this.eMdl);
+            // sap.ui.getCore().applyTheme("sap_horizon");
+            // //value state removing if existing state is thare means
+            // this.eMdl = this.getOwnerComponent().getModel('errors');
+            // ErrorMessage.removeValueState(this.formId, this.eMdl);
         },
         onAfterRendering: function () {
             var view = this.getView();
@@ -63,22 +64,23 @@ sap.ui.define([
             var that = this;
             var cModel = this.getView().getModel('loginModel');
             var enableProp = cModel.getData().enable;
-            //Error popover
-            ErrorMessage.formValidation(this.formId, this.eMdl, this.pageId);
-            //
-            var valid = this.eMdl.getData()
-            if (valid.length == 0) {
-                this.errorMessagePopoverClose()
-                if (enableProp == true) {
-                    cModel.getData().enable = false;
-                    cModel.refresh();
-                    this.loginBtn.setText("Verify Token")
-                } else {
-                    this.getRouter().navTo("dashboard");
-                }
+            var emailvalidate = cModel.getData().email;
+            var mailregex = /^\w+[\w-+\.]*\@\w+([-\.]\w+)*\.[a-zA-Z]{2,}$/;
+            !emailvalidate.match(mailregex) ? (cModel.getData().emailValueState = "Error", cModel.getData().emailValueStateText = "Invalid Email") : cModel.getData().emailValueState = "None";
+            var passvalidate = cModel.getData().password;
+            !passvalidate.match(mailregex) ? (cModel.getData().passValueState = "Error", cModel.getData().passValueStateText = "Invalid Email") : cModel.getData().passValueState = "None";
+            cModel.refresh();
+            if (enableProp == true) {
+                cModel.getData().enable = false;
+                cModel.refresh();
+                // this.loginBtn.setText("Verify Token")
+                MessageToast.show("Please Enter correct UserName and Password");
             } else {
-                //this.errorMessagePopover(this.popoverBtn);
+                this.getRouter().navTo("dashboard");
             }
+            // } else {
+            //     //this.errorMessagePopover(this.popoverBtn);
+            // }
         }
     });
 });
